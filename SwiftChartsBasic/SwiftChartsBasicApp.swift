c02d86c2a96f2f3711f819baa3d1dfe93992976b
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-10-14 08:08:50 -0500

    Initial Commit

---
+++ SwiftChartsBasic/SwiftChartsBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsBasicApp.swift
+//  SwiftChartsBasic
+//
+//  Created by Mark Volkmann on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
